aed1011f7cbc172fec1b1abe62138c2d0bbec869
Karlen999 <69694626+Karlen999@users.noreply.github.com>
2020-11-11 12:14:59 +0400

    Spring MVC

---
+++ src/main/java/com/bdg/springdemo/controller/MainController.java
@@ -0,0 +1,47 @@
+package com.bdg.springdemo.controller;
+
+import com.bdg.springdemo.model.User;
+import com.bdg.springdemo.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+
+@Controller
+public class MainController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @GetMapping("/")
+    public String homePage(ModelMap modelMap) {
+        List<User> all = userRepository.findAll();
+        modelMap.addAttribute("users",all);
+        modelMap.addAttribute("msg","Welcome");
+        return "home";
+    }
+
+    @PostMapping("/addUser")
+    public String addUser(@ModelAttribute User user){
+        userRepository.save(user);
+        return "redirect:/";
+    }
+
+    @GetMapping("/deleteUser")
+    public String deleteUser(@RequestParam("id") int id){
+        userRepository.deleteById(id);
+        return "redirect:/";
+
+    }
+
+
+    @GetMapping("/about")
+    public String aboutUsPage(){
+        return "about";
+    }
+}
